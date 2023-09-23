3b92712a9a56930d1086f23af975747e7cc0007c
unknown <maryamnzlawy@gmail.coom>
2023-09-23 13:39:52 +0300

    remove duplications

--- PacketsManager.h
+++ PacketsManager.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include <filesystem>
 #ifndef PROJECT1_CPPV2_PACKETSMANAGER_H
@@ -26,29 +25,3 @@ public:
 
 
 #endif //PROJECT1_CPPV2_PACKETSMANAGER_H
-=======
-#include <iostream>
-#include <filesystem>
-#ifndef PROJECT1_CPPV2_PACKETSMANAGER_H
-#define PROJECT1_CPPV2_PACKETSMANAGER_H
-
-namespace fs = std::filesystem;
-class PacketsManager {
-private:
-    uintmax_t disk_size;
-    uintmax_t remaining_disk_size;
-    std::string directoryPath = R"(D:\Internships\VIAVI Internship 2023\Testing Folder)";
-public:
-    void set_diskSize(uintmax_t size);
-    uintmax_t get_diskSize();
-    void read();
-    void check(uintmax_t s,std::vector<fs::directory_entry>& file);
-    static bool compareByModTime(const fs::directory_entry& a, const fs::directory_entry& b);
-    void sort(std::vector<fs::directory_entry>& file);
-    void remove(std::vector<fs::directory_entry>& file);
-
-};
-
-
-#endif //PROJECT1_CPPV2_PACKETSMANAGER_H
->>>>>>> origin/main
